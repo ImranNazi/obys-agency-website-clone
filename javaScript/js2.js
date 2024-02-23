5f83ca9dc414a0b74fb9a902a36d22bcd878f802
ImranNazi <157121434+ImranNazi@users.noreply.github.com>
2024-02-23 12:54:04 +0530

    Delete javaScript directory

--- javaScript/js2.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector("button");
-btn.addEventListener("mouseover", function () {
-  btn.textContent = "starting...";
-  btn.style.backgroundColor = "cyan";
-});
-
-btn.addEventListener("mouseleave", function () {
-  alert();
-});
